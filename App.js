99cd89740c6af6e5c02d57d723ad3b0d27bd76e2
Ajten <ajtencani@gmail.com>
2021-12-28 23:43:35 +0100

    finished loading fonts in app.js

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import {useState} from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import useFonts from './hooks/useFont';
+import AppLoading from 'expo-app-loading';
+
 
 
 export default function App() {
+  const [IsReady, SetIsReady] = useState(false);
+
+  const LoadFonts = async () => {
+    await useFonts();
+  }
+
+  if (!IsReady) {
+    return (
+        <AppLoading
+            startAsync={LoadFonts}
+            onFinish={() => SetIsReady(true)}
+            onError={() => {
+            }}
+        />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.testo}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,4 +39,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  testo : {
+    fontFamily: 'OpenSans-Regular'
+  }
 });
