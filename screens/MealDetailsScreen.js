9904a7010e5b4b3f63207ff91c39b03b3b0e7576
Ajten <ajtencani@gmail.com>
2021-12-29 01:56:28 +0100

    added a go back to begin history button

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,8 +1,19 @@
 import React from "react";
-import {StyleSheet,View,Text} from "react-native";
+import {StyleSheet,View,Text,Button} from "react-native";
 
-const MealDetailsScreen = () => {
-    return(<View style={styles.screen}><Text>The Meal details screen</Text></View>)
+const MealDetailsScreen = (props) => {
+    return(
+        <View style={styles.screen}>
+            <Text>The Meal details screen</Text>
+            <View style={styles.button}>
+                <Button title='Go back to categories' onPress={
+                    () => {
+                        props.navigation.popToTop()
+                    }
+                }/>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -10,6 +21,10 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent:'center',
         alignItems : 'center'
+    },
+    button: {
+        padding: 20,
+        marginTop: 10,
     }
 })
 
