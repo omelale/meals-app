02d1241f1985f9d5f5718f70f132d93e90f6036a
Ajten <ajtencani@gmail.com>
2021-12-29 01:46:23 +0100

    Added navigation to 3 pages

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,29 @@
 import React from "react";
-import {StyleSheet,View,Text} from "react-native";
+import {Button, StyleSheet, Text, View} from "react-native";
 
-const CategoriesScreen = () => {
-    return(<View style={styles.screen}><Text>The categories screen</Text></View>)
+const CategoriesScreen = ({navigation}) => {
+    return (
+        <View style={styles.screen}>
+            <Text>The categories screen</Text>
+            <View style={styles.button}>
+                <Button title='Go to meals' onPress={
+                    () => {
+                        navigation.navigate('CategoryMeals')
+                    }
+                }/>
+            </View>
+        </View>)
 }
 
 const styles = StyleSheet.create({
-    screen : {
-        flex:1,
-        justifyContent:'center',
-        alignItems : 'center'
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        padding: 20,
+        marginTop: 10,
     }
 })
 
--- screens/CategoryMeals.js
+++ screens/CategoryMeals.js
@@ -1,15 +1,29 @@
 import React from "react";
-import {StyleSheet,View,Text} from "react-native";
+import {StyleSheet, Text, View, Button} from "react-native";
 
-const CategoryMeals = () => {
-    return(<View style={styles.screen}><Text>The category meals screen</Text></View>)
+const CategoryMeals = (props) => {
+    return (
+        <View style={styles.screen}>
+            <Text>The category meals screen</Text>
+            <View style={styles.button}>
+                <Button title='Check this meal out' onPress={
+                    () => {
+                        props.navigation.navigate('MealDetailsScreen')
+                    }
+                }/>
+            </View>
+        </View>)
 }
 
 const styles = StyleSheet.create({
-    screen : {
-        flex:1,
-        justifyContent:'center',
-        alignItems : 'center'
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        padding: 20,
+        marginTop: 10,
     }
 })
 
