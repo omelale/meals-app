848c64c733b53d26b75c5560b78af147bb6635bc
Ajten <ajtencani@gmail.com>
2021-12-30 19:49:38 +0100

    added dummy data config

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,20 @@
 import React from "react";
-import {Button, StyleSheet, Text, View} from "react-native";
+import {Button, StyleSheet, Text, View , FlatList} from "react-native";
 
 const CategoriesScreen = ({navigation}) => {
     return (
-        <View style={styles.screen}>
-            <Text>The categories screen</Text>
-            <View style={styles.button}>
-                <Button title='Go to meals' onPress={
-                    () => {
-                        navigation.navigate('CategoryMeals')
-                    }
-                }/>
-            </View>
-        </View>)
+        // <View style={styles.screen}>
+        //     <Text>The categories screen</Text>
+        //     <View style={styles.button}>
+        //         <Button title='Go to meals' onPress={
+        //             () => {
+        //                 navigation.navigate('CategoryMeals')
+        //             }
+        //         }/>
+        //     </View>
+        // </View>
+        <FlatList numColumns={2}/>
+    )
 }
 
 const styles = StyleSheet.create({
