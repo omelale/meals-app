f723914f098ef3267dbf3f2a71073bf31fe02e5d
Ajten <ajtencani@gmail.com>
2021-12-31 00:45:07 +0100

    loaded meals const to the screen

--- screens/CategoryMeals.js
+++ screens/CategoryMeals.js
@@ -1,28 +1,28 @@
 import React, {useEffect} from "react";
-import {StyleSheet, Text, View, Button} from "react-native";
-import {CATEGORIES} from "../data/dummy-data";
+import {Button, StyleSheet, Text, View} from "react-native";
+import {CATEGORIES, MEALS} from "../data/dummy-data";
 
 
 const CategoryMeals = (props) => {
     const catId = props.route.params.categoryId;
-    if(catId == undefined){
-        return  (<Text>Go back cuz route not ok</Text>)
+    if (catId == undefined) {
+        return (<Text>Go back cuz route not ok</Text>)
     }
     const category = CATEGORIES.find(cat => cat.id === catId);
-    useEffect(()=>{
-            props.navigation.setOptions({ title: category.title })
+    const meals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+    useEffect(() => {
+        props.navigation.setOptions({title: category.title})
     })
     return (
         <View style={styles.screen}>
             <Text>Here you will find all the meals of the {category.title} category</Text>
             <View style={styles.button}>
-                <Button title='Check this meal out' onPress={
-                    () => {
-                        props.navigation.navigate('MealDetailsScreen')
-                    }
-                }/>
+                <Button title='Check this meal out' onPress={() => {
+                    props.navigation.navigate('MealDetailsScreen')
+                }}/>
             </View>
-        </View>)
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
