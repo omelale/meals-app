fb02e8bcee8b3616476faf488cd1ffb507a96225
Ajten <ajtencani@gmail.com>
2021-12-31 00:55:31 +0100

    added new component for meals and loaded wit initial data

---
+++ components/MealGridTile.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {View,Text} from "react-native";
+
+const MealGridTile = (props) => {
+    return (<View><Text>{props.title}</Text></View>)
+}
+
+export default MealGridTile
--- screens/CategoryMeals.js
+++ screens/CategoryMeals.js
@@ -1,39 +1,44 @@
 import React, {useEffect} from "react";
-import {Button, StyleSheet, Text, View} from "react-native";
+import { StyleSheet, FlatList} from "react-native";
 import {CATEGORIES, MEALS} from "../data/dummy-data";
+import MealGridTile from "../components/MealGridTile";
 
 
 const CategoryMeals = (props) => {
     const catId = props.route.params.categoryId;
-    if (catId == undefined) {
-        return (<Text>Go back cuz route not ok</Text>)
-    }
     const category = CATEGORIES.find(cat => cat.id === catId);
     const meals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+    const renderMeal = (itemData) => {
+        return (
+            <MealGridTile
+                title={itemData.item.title}
+                affordability={itemData.item.affordability}
+                onTouch={() => {
+                props.navigation.navigate('CategoryMeals',{categoryId: itemData.item.id});
+            }}/>
+        )
+    }
+
     useEffect(() => {
         props.navigation.setOptions({title: category.title})
     })
     return (
-        <View style={styles.screen}>
-            <Text>Here you will find all the meals of the {category.title} category</Text>
-            <View style={styles.button}>
-                <Button title='Check this meal out' onPress={() => {
-                    props.navigation.navigate('MealDetailsScreen')
-                }}/>
-            </View>
-        </View>
+        <FlatList data={meals} renderItem={renderMeal} numColumns={1} style={styles.screen}/>
+        // <View style={styles.screen}>
+        //     <Text>Here you will find all the meals of the {category.title} category</Text>
+        //     <View style={styles.button}>
+        //         <Button title='Check this meal out' onPress={() => {
+        //             props.navigation.navigate('MealDetailsScreen')
+        //         }}/>
+        //     </View>
+        // </View>
     )
 }
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    button: {
-        padding: 20,
-        marginTop: 10,
+        padding:10
     }
 })
 
