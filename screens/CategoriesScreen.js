7a929a542da7403b43434161e3a3b8e02be8cb8b
Ajten <ajtencani@gmail.com>
2021-12-30 20:00:44 +0100

    imported dummy data

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Button, StyleSheet, Text, View , FlatList} from "react-native";
+import {CATEGORIES} from "../data/dummy-data";
+
 
 const CategoriesScreen = ({navigation}) => {
     return (
