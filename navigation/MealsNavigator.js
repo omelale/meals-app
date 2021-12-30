32359d115f09152ce137eb8010ac4dc4779578cc
Ajten <ajtencani@gmail.com>
2021-12-30 22:55:32 +0100

    added extra styles

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -2,6 +2,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMeals from "../screens/CategoryMeals";
 import MealDetailsScreen from "../screens/MealDetailsScreen";
+import {StyleSheet} from "react-native";
 import colors from "../constants/colors";
 
 const Stack = createStackNavigator();
@@ -12,22 +13,40 @@ function NewNav() {
             name="CategoriesScreen"
             component={CategoriesScreen}
             options={{
-                title: 'Categories', headerStyle: {
-                    backgroundColor: colors.primaryColor,
-                }, headerTintColor: '#fff', headerTitleStyle: {
-                    fontWeight: 'bold',
-                }
+                title: 'Categories',
+                headerStyle: styles.headerStyle,
+                headerTintColor: '#fff',
+                headerTitleStyle: styles.headerTitleStyle
             }}
         />
-        <Stack.Screen name="CategoryMeals" component={CategoryMeals} options={{
-                headerStyle: {
-                    backgroundColor: colors.primaryColor,
-                }, headerTintColor: '#fff', headerTitleStyle: {
-                    fontWeight: 'bold',
-                }
-            }} />
-        <Stack.Screen name="MealDetailsScreen" component={MealDetailsScreen}/>
+        <Stack.Screen
+            name="CategoryMeals"
+            component={CategoryMeals}
+            options={{
+                headerStyle: styles.headerStyle,
+                headerTintColor: '#fff',
+                headerTitleStyle: styles.headerTitleStyle
+            }}
+        />
+        <Stack.Screen
+            name="MealDetailsScreen"
+            component={MealDetailsScreen}
+            options={{
+                headerStyle: styles.headerStyle,
+                headerTintColor: '#fff',
+                headerTitleStyle: styles.headerTitleStyle
+            }}
+        />
     </Stack.Navigator>);
 }
+
+const styles = StyleSheet.create({
+    headerStyle: {
+        backgroundColor: colors.primaryColor,
+    },
+    headerTitleStyle: {
+        fontWeight: 'bold',
+    }
+})
 
 export default NewNav
