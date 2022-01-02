4537d4145d8c45efb7670cfe7588ced46a72b433
Ajten <ajtencani@gmail.com>
2022-01-02 23:00:05 +0100

    refactor

--- hooks/useFont.js
+++ hooks/useFont.js
@@ -3,7 +3,7 @@ import * as Font from "expo-font";
 let useFonts;
 
 export default useFonts = async () =>
-  await Font.loadAsync({
-    'OpenSans-Regular': require('../assets/fonts/OpenSans-Regular.ttf'),
-    'OpenSans-Bold': require('../assets/fonts/OpenSans-Bold.ttf')
-  });
+    await Font.loadAsync({
+        'OpenSans-Regular': require('../assets/fonts/OpenSans-Regular.ttf'),
+        'OpenSans-Bold': require('../assets/fonts/OpenSans-Bold.ttf')
+    });
