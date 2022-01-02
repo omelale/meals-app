49d3c3a450b16276bf0688385f614e4be4d87346
Ajten <ajtencani@gmail.com>
2022-01-02 22:59:22 +0100

    refactor

--- models/Category.js
+++ models/Category.js
@@ -1,5 +1,5 @@
-class Category{
-    constructor(id,title,color) {
+class Category {
+    constructor(id, title, color) {
         this.id = id;
         this.title = title;
         this.color = color;
