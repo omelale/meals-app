f82f75838537b2b93850bc91d185dd91b6a1abbf
Ajten <ajtencani@gmail.com>
2022-01-09 00:22:12 +0100

    added the reducer needed to toggle favourites

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,5 +1,6 @@
 // this file will hold the reducers that include the logic of storing the state of
 import {MEALS} from "../../data/dummy-data";
+import {ADDTOFAVOURITES} from '../actions/meals';
 
 const initialState = {
     meals: MEALS,
@@ -8,7 +9,20 @@ const initialState = {
 }
 
 const mealsReducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case 'ADDTOFAVOURITES' : {
+            const existingIndex = state.favouriteMeals.findIndex(meal => meal.id === action.mealId);
+            if(existingIndex >= 0){
+                //need to remove the meal from favourites
+                const newFavs = [...state.favouriteMeals].splice(existingIndex,1);
+                return {...state, favouriteMeals: newFavs}
+            } else {
+                return {...state,favouriteMeals: state.favouriteMeals.concat(state.meals.find(meal=>meal.id===action.mealId))}
+            }
+        }
+        default :
+            return state;
+    }
 }
 
 export default mealsReducer;
